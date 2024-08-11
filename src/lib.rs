1710443df3533aed848f5c11fb0b9e55719490b4
June Park <junepark202012@gmail.com>
2024-08-11 16:01:03 +0900

    Remove irrelevant sample code

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,11 @@
+#[cfg(test)]
+pub use test_utils::TestContext;
+
 pub mod client;
 mod config;
 pub mod endpoints;
 mod error;
-
-#[cfg(test)]
-mod test_utils;
 mod types;
 
 #[cfg(test)]
-pub use test_utils::TestContext;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod test_utils;
