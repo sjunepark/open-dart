433e685b16e0bf5235246c566cd42c10e78ba406
June Park <junepark202012@gmail.com>
2024-09-11 15:00:31 +0900

    feat: corp_cls

--- src/types/corp_cls.rs
+++ src/types/corp_cls.rs
@@ -1,11 +1,8 @@
 use self::Inner::*;
-use crate::assert_impl_all_commons;
 use crate::error::OpenDartError;
 use derive_more::{AsMut, AsRef, Display, FromStr};
 use serde::{Deserialize, Serialize};
 use std::fmt::Formatter;
-
-assert_impl_all_commons!(CorpCls);
 
 /// ### 법인구분
 ///
@@ -15,25 +12,19 @@ assert_impl_all_commons!(CorpCls);
 /// - E : 기타
 ///
 /// ※ 없으면 전체조회, 복수조건 불가
-#[derive(
-    Clone, Eq, PartialEq, Ord, PartialOrd, Debug, Default, Serialize, Deserialize, AsRef, AsMut,
-)]
-pub struct CorpCls(Option<Inner>);
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Debug, Serialize, Deserialize, AsRef, AsMut)]
+pub struct CorpCls(Inner);
 
 impl CorpCls {
-    pub const Y: Self = Self(Some(Y));
-    pub const K: Self = Self(Some(K));
-    pub const N: Self = Self(Some(N));
-    pub const E: Self = Self(Some(E));
-    pub const NONE: Self = Self(None);
+    pub const Y: Self = Self(Y);
+    pub const K: Self = Self(K);
+    pub const N: Self = Self(N);
+    pub const E: Self = Self(E);
 }
 
 impl Display for CorpCls {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match &self.0 {
-            Some(inner) => inner.fmt(f),
-            None => write!(f, "None"),
-        }
+        write!(f, "{}", self.as_ref())
     }
 }
 
@@ -46,7 +37,6 @@ impl FromStr for CorpCls {
             "K" => Ok(Self::K),
             "N" => Ok(Self::N),
             "E" => Ok(Self::E),
-            "" => Ok(Self::NONE),
             _ => Err(OpenDartError::InvalidArgument(s.to_string())),
         }
     }
@@ -68,11 +58,6 @@ mod tests {
     use super::*;
 
     #[test]
-    fn default() {
-        assert_eq!(CorpCls::default(), CorpCls::NONE);
-    }
-
-    #[test]
     fn serialize() {
         assert_eq!(
             serde_json::to_string(&CorpCls::Y).expect("Failed to serialize"),
@@ -89,10 +74,6 @@ mod tests {
         assert_eq!(
             serde_json::to_string(&CorpCls::E).expect("Failed to serialize"),
             r#""E""#
-        );
-        assert_eq!(
-            serde_json::to_string(&CorpCls::NONE).expect("Failed to serialize"),
-            r#"null"#
         );
     }
 
@@ -114,10 +95,6 @@ mod tests {
             serde_json::from_str::<CorpCls>(r#""E""#).expect("Failed to deserialize"),
             CorpCls::E
         );
-        assert_eq!(
-            serde_json::from_str::<CorpCls>(r#"null"#).expect("Failed to deserialize"),
-            CorpCls::NONE
-        );
     }
 
     #[test]
@@ -126,7 +103,6 @@ mod tests {
         assert_eq!(CorpCls::K.to_string(), "K");
         assert_eq!(CorpCls::N.to_string(), "N");
         assert_eq!(CorpCls::E.to_string(), "E");
-        assert_eq!(CorpCls::NONE.to_string(), "None");
     }
 
     #[test]
@@ -135,6 +111,13 @@ mod tests {
         assert!(matches!("K".parse::<CorpCls>(), Ok(CorpCls::K)));
         assert!(matches!("N".parse::<CorpCls>(), Ok(CorpCls::N)));
         assert!(matches!("E".parse::<CorpCls>(), Ok(CorpCls::E)));
-        assert!(matches!("".parse::<CorpCls>(), Ok(CorpCls::NONE)));
+    }
+
+    #[test]
+    fn display_inner() {
+        assert_eq!(Y.to_string(), "Y");
+        assert_eq!(K.to_string(), "K");
+        assert_eq!(N.to_string(), "N");
+        assert_eq!(E.to_string(), "E");
     }
 }
